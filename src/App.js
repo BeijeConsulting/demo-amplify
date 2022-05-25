99244b43ebb0a87c1067a418c00cd01ae04514a5
ivfly <iv0fly@gmail.com>
2022-05-25 11:24:36 +0200

    demo for AWS Amplify

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './beije.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.beije.it/it/beije-talent-academy/academy-frontend"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React (in the Beije Talent Academy)
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
